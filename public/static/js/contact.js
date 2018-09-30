4d18f29db24f9d848b92fc79e902b42b9e59e751
Gabriel <gabi_srm@hotmail.com>
2018-09-30 20:21:42 +0200

    Cambios

--- public/static/js/contact.js
+++ public/static/js/contact.js
@@ -46,11 +46,10 @@ $(document).ready(function () {
 	});
 
 	initMenu();
-	initGoogleMap();
 
 
 
-	$('#contact_form').submit(function (event) {
+	$('#contact_form, #footer_contact_form').submit(function (event) {
 		event.preventDefault();
 		var mail = $('#contact_email').val();
 		console.log("Mail:", mail);
@@ -161,49 +160,6 @@ $(document).ready(function () {
 
 	*/
 
-	function initGoogleMap() {
-		var myLatlng = new google.maps.LatLng(34.063685, -118.272936);
-		var mapOptions =
-		{
-			center: myLatlng,
-			zoom: 14,
-			mapTypeId: google.maps.MapTypeId.ROADMAP,
-			draggable: true,
-			scrollwheel: false,
-			zoomControl: true,
-			zoomControlOptions:
-			{
-				position: google.maps.ControlPosition.RIGHT_CENTER
-			},
-			mapTypeControl: false,
-			scaleControl: false,
-			streetViewControl: false,
-			rotateControl: false,
-			fullscreenControl: true,
-			styles:
-				[
-					{
-						"featureType": "road.highway",
-						"elementType": "geometry.fill",
-						"stylers": [
-							{
-								"color": "#ffeba1"
-							}
-						]
-					}
-				]
-		}
 
-		// Initialize a map with options
-		map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-		// Re-center map after window resize
-		google.maps.event.addDomListener(window, 'resize', function () {
-			setTimeout(function () {
-				google.maps.event.trigger(map, "resize");
-				map.setCenter(myLatlng);
-			}, 1400);
-		});
-	}
 
 });
